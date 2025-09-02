3ac58e881f776aa8cf48fb2a09a0fb5715a7c82b
unknown <adepuramya32@gmail.com>
2025-09-02 13:18:14 +0530

    Test Harness webhook trigger

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  
 # Hardcoded Splunk HEC values
-SPLUNK_URL="https://prd-p-idagf.splunkcloud.com"
-HEC_TOKEN="18e2d830-de74-404b-a4ab-3e7f21940638"
+SPLUNK_URL="https://prd-p-idagf.splunkcloud.com:8088"
+HEC_TOKEN="a593068d-c9ac-4608-b36c-13708c8d40b2"
   
 # Specify the log file and source ltype directly here
 LOGFILE="logs/application.log"         # ✅ Change this to your desired log filess
 SOURCETYPE="application_logs"               # ✅ Change this to your desired source type
-INDEX="my_harness_index"     # ✅ Change this to your desired index
+INDEX="ramya-index"     # ✅ Change this to your desired index
  
 # Debug info
 echo "Sending logs to: $SPLUNK_URL"
